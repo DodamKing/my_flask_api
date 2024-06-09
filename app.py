f03e401518bbc7f0cb804984e566e9d8dd857990
Dodamking <power6120@naver.com>
2024-06-09 20:57:36 +0900

    cors 설정

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from rebalancing_calculator.routes import rc
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(rc, url_prefix='/api/rebalancing-calculator')
 
