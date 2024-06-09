7129853478876167fccf9621cae5ec761dfa4e7b
Dodamking <power6120@naver.com>
2024-06-09 16:46:34 +0900

    오류 수정 및 통합 api 추가

--- rebalancing_calculator/routes.py
+++ rebalancing_calculator/routes.py
@@ -13,8 +13,28 @@ def search_stock(query: str):
 
 @rc.get("/price/<symbol>")
 def get_stock_price(symbol: str):
-    stocks = fdr.StockListing('ETF/KR')
+    exchange = request.args.get('exchange', 'ETF/KR')
+    stocks = fdr.StockListing(exchange)
     stock = stocks[stocks['Symbol'] == symbol]
-    price = stock['Price'].iloc[0]
-    
+    price = int(stock['Price'].iloc[0])
+
     return {"symbol": symbol, "price": price}
+
+@rc.get("/search/<by>/<query>")
+def get_stocks(by, query: str):
+    exchange = request.args.get('exchange', 'ETF/KR')
+    stocks = fdr.StockListing(exchange)
+
+    if by == 'name':
+        result = stocks[stocks['Name'].str.contains(query, case=False, na=False)]
+
+        return result.to_json(orient='records', force_ascii=False)
+
+    elif by == 'symbol':
+        stock = stocks[stocks['Symbol'] == query]
+
+        if not stock.empty:
+            price = int(stock['Price'].iloc[0])
+            return {"symbol": query, "price": price}
+        else:
+            return 'no symbol'
